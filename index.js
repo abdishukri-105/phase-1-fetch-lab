9a54973fa013848d1d4efda9266eb33ecd9724cb
abdishukri <abdishukrimohamed105@gmail.com>
2022-12-05 19:24:50 +0300

    completed

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((res) =>res.json())
+  .then( books => {
+    books.forEach(() => {
+      renderBooks(books)
+    })
+  })
 }
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
